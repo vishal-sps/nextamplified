5dcd906b079f5a6ba9b223d685f5fffdc0f58867
vishal-sps <vishal_modanwal@softprodigy.com>
2023-06-28 15:18:24 +0530

    first commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,20 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "nextamplified": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "nextamplified7a907afd": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import '@aws-amplify/ui-react/styles.css';
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
